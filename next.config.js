3b1e62c26e307997998c024c94b8f71c26bf6ee7
Hazmi Abdun Nazir <hazmi.abdun@bukalapak.com>
2021-02-07 11:05:37 +0700

    add optimized image page

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  images: {
+    domains: [
+      'media0.giphy.com',
+      'media1.giphy.com',
+      'media2.giphy.com',
+      'media3.giphy.com',
+      'media4.giphy.com',
+      'media5.giphy.com',
+    ],
+  },
+}
---
+++ pages/optimized.js
@@ -0,0 +1,67 @@
+import { useEffect, useState } from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
+import Image from 'next/image';
+import { prepareGifList } from '../helpers/prepareGifList';
+import styles from '../styles/gif.module.css'
+
+export default function Home({ gifs }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>SSR Rendering with Optimized Image - Perf Comparison</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header className={styles.header}>
+        <h1>Optimized Server Side Rendering (SSR) Page</h1>
+        <Link href="/csr">
+          <a>Go to Client Side Rendering (CSR) Page</a>
+        </Link> 
+      </header>
+      <main>
+        <h2>Trending GIF's in GIPHY</h2>
+        <div
+          className={styles.wrapper}
+          style={{
+            height: gifs.containerHeight
+          }}
+        >
+          {gifs.data.map((gif) => {
+            return (
+              <section
+                key={gif.id}
+                className={styles.gif}
+                style={gif.style}
+              >
+                <h4>{gif.title}</h4>
+                <Image {...gif.imgAttr} />
+                {gif.userDisplayName ? (
+                  <aside className={styles.user}>
+                    <Image {...gif.userImgAttr} />
+                    <h5>{gif.userDisplayName}</h5>
+                  </aside>
+                ) : null}
+              </section>
+            )
+          })}
+        </div>
+      </main>
+      <footer>
+        <p>This is the end of the page.</p>
+        <p>Hazmi</p>
+      </footer>
+    </div>
+  )
+}
+
+export async function getStaticProps() {
+  const res = await fetch('https://api.giphy.com/v1/gifs/trending?api_key=xpdrHwpGnEbznpNzYnB4bSAuDAadz8tO')
+  const rawGifs = await res.json()
+
+  const gifs = prepareGifList(rawGifs.data);
+  return {
+    props: {
+      gifs,
+    },
+  }
+}
