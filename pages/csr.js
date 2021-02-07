49ad9f7620cf944dd2ff821428ef3d4042f05e00
Hazmi Abdun Nazir <hazmi.abdun@bukalapak.com>
2021-02-07 10:39:24 +0700

    change url and add semantic html

--- pages/csr.js
+++ pages/csr.js
@@ -8,7 +8,7 @@ export default function Home() {
   const [gifs, setGifs] = useState({ data: [], containerHeight: 0 });
   
   useEffect(() => {
-    fetch('http://api.giphy.com/v1/gifs/trending?api_key=xpdrHwpGnEbznpNzYnB4bSAuDAadz8tO')
+    fetch('https://api.giphy.com/v1/gifs/trending?api_key=xpdrHwpGnEbznpNzYnB4bSAuDAadz8tO')
       .then(res => res.json())
       .then(res => {
         const gifs = prepareGifList(res.data);
@@ -22,38 +22,44 @@ export default function Home() {
         <title>CSR Rendering - Perf Comparison</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className={styles.header}>
+      <header className={styles.header}>
         <h1>Client Side Rendering (CSR) Page</h1>
         <Link href="/ssr">
           <a>Go to Server Side Rendering (SSR) Page</a>
         </Link> 
-      </div>
-      <h2>Trending GIF's in GIPHY</h2>
-      <div
-        className={styles.wrapper}
-        style={{
-          height: gifs.containerHeight
-        }}
-      >
-        {gifs.data.map((gif) => {
-          return (
-            <section
-              key={gif.id}
-              className={styles.gif}
-              style={gif.style}
-            >
-              <h4>{gif.title}</h4>
-              <img {...gif.imgAttr} />
-              {gif.userDisplayName ? (
-                <aside className={styles.user}>
-                  <img {...gif.userImgAttr} />
-                  <h5>{gif.userDisplayName}</h5>
-                </aside>
-              ) : null}
-            </section>
-          )
-        })}
-      </div>
+      </header>
+      <main>
+        <h2>Trending GIF's in GIPHY</h2>
+        <div
+          className={styles.wrapper}
+          style={{
+            height: gifs.containerHeight
+          }}
+        >
+          {gifs.data.map((gif) => {
+            return (
+              <section
+                key={gif.id}
+                className={styles.gif}
+                style={gif.style}
+              >
+                <h4>{gif.title}</h4>
+                <img {...gif.imgAttr} />
+                {gif.userDisplayName ? (
+                  <aside className={styles.user}>
+                    <img {...gif.userImgAttr} />
+                    <h5>{gif.userDisplayName}</h5>
+                  </aside>
+                ) : null}
+              </section>
+            )
+          })}
+        </div>
+      </main>
+      <footer>
+        <p>This is the end of the page.</p>
+        <p>Hazmi</p>
+      </footer>
     </div>
   )
 }
--- pages/ssr.js
+++ pages/ssr.js
@@ -11,44 +11,50 @@ export default function Home({ gifs }) {
         <title>SSR Rendering - Perf Comparison</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className={styles.header}>
+      <header className={styles.header}>
         <h1>Server Side Rendering (SSR) Page</h1>
         <Link href="/csr">
           <a>Go to Client Side Rendering (CSR) Page</a>
         </Link> 
-      </div>
-      <h2>Trending GIF's in GIPHY</h2>
-      <div
-        className={styles.wrapper}
-        style={{
-          height: gifs.containerHeight
-        }}
-      >
-        {gifs.data.map((gif) => {
-          return (
-            <section
-              key={gif.id}
-              className={styles.gif}
-              style={gif.style}
-            >
-              <h4>{gif.title}</h4>
-              <img {...gif.imgAttr} />
-              {gif.userDisplayName ? (
-                <aside className={styles.user}>
-                  <img {...gif.userImgAttr} />
-                  <h5>{gif.userDisplayName}</h5>
-                </aside>
-              ) : null}
-            </section>
-          )
-        })}
-      </div>
+      </header>
+      <main>
+        <h2>Trending GIF's in GIPHY</h2>
+        <div
+          className={styles.wrapper}
+          style={{
+            height: gifs.containerHeight
+          }}
+        >
+          {gifs.data.map((gif) => {
+            return (
+              <section
+                key={gif.id}
+                className={styles.gif}
+                style={gif.style}
+              >
+                <h4>{gif.title}</h4>
+                <img {...gif.imgAttr} />
+                {gif.userDisplayName ? (
+                  <aside className={styles.user}>
+                    <img {...gif.userImgAttr} />
+                    <h5>{gif.userDisplayName}</h5>
+                  </aside>
+                ) : null}
+              </section>
+            )
+          })}
+        </div>
+      </main>
+      <footer>
+        <p>This is the end of the page.</p>
+        <p>Hazmi</p>
+      </footer>
     </div>
   )
 }
 
 export async function getStaticProps() {
-  const res = await fetch('http://api.giphy.com/v1/gifs/trending?api_key=xpdrHwpGnEbznpNzYnB4bSAuDAadz8tO')
+  const res = await fetch('https://api.giphy.com/v1/gifs/trending?api_key=xpdrHwpGnEbznpNzYnB4bSAuDAadz8tO')
   const rawGifs = await res.json()
 
   const gifs = prepareGifList(rawGifs.data);
