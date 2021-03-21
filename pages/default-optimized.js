c2082fd690e048f99bcbd8ef3dd305711fadfd92
Hazmi Abdun Nazir <hazmi.abdun@bukalapak.com>
2021-03-21 15:21:18 +0700

    rename CSR to Default so its not missleading

--- pages/default-optimized.js
+++ pages/default-optimized.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
+import Image from "next/image";
 import Head from "next/head";
-import MobileDetect from 'mobile-detect';
+import MobileDetect from "mobile-detect";
 import { prepareGifList } from "../helpers/prepareGifList";
 import styles from "../styles/gif.module.css";
 
@@ -8,16 +9,18 @@ export default function CSR() {
   const [gifs, setGifs] = useState({ data: [], containerHeight: 0 });
 
   useEffect(() => {
-    fetch('https://api.giphy.com/v1/gifs/trending?api_key=xpdrHwpGnEbznpNzYnB4bSAuDAadz8tO')
-      .then(res => res.json())
-      .then(rawGifs => {
+    fetch(
+      "https://api.giphy.com/v1/gifs/trending?api_key=xpdrHwpGnEbznpNzYnB4bSAuDAadz8tO"
+    )
+      .then((res) => res.json())
+      .then((rawGifs) => {
         const md = new MobileDetect(navigator.userAgent);
         let gifs;
-        if(md.mobile()) {
+        if (md.mobile()) {
           gifs = prepareGifList(rawGifs.data, {
             gutter: 14,
             width: 158,
-            defaultColumn: [0,0]
+            defaultColumn: [0, 0],
           });
         } else {
           gifs = prepareGifList(rawGifs.data);
@@ -30,26 +33,23 @@ export default function CSR() {
     <div>
       <div className={styles.container}>
         <Head>
-          <title>CSR in Next.js v10.0.2 Performance Comparison</title>
+          <title>Default (CSR with Pre-renders) + Optimized Image in Next.js v10.0.2 Performance Comparison</title>
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <header className={styles.header}>
-          <h1>Client Side Rendering (CSR)</h1>
+          <h1>Default (CSR with Pre-renders) + Image Optmization</h1>
           <p>
-            Default Client Side Rendering (CSR) page. All API calls and
-            JavaScript processes happen in the browsers. Although, in Next.js,
-            this is not actually a pure CSR since, by default,{" "}
-            <a href="https://nextjs.org/docs/basic-features/pages">
-              Next.js pre-renders every page
-            </a>
-            . They have{" "}
-            <a href="https://nextjs.org/docs/advanced-features/automatic-static-optimization">
-              Automatic Static Optimization
-            </a>.
+            This version uses the Default (CSR with Pre-renders) version.
+            Additionally, this version does an{" "}
+            <a href="https://nextjs.org/docs/basic-features/image-optimization">
+              image optimization
+            </a>{" "}
+            using the <code>&lt;Image /&gt;</code> component exported by{" "}
+            <code>next/image</code>.
           </p>
         </header>
         <main>
-          <h2>Trending GIF's in GIPHY</h2>
+          <h2>Trending GIF's in <a href="https://giphy.com">GIPHY</a></h2>
           <div
             className={styles.wrapper}
             style={{
@@ -60,10 +60,10 @@ export default function CSR() {
               return (
                 <section key={gif.id} className={styles.gif} style={gif.style}>
                   <h4>{gif.title}</h4>
-                  <img {...gif.imgAttr} />
+                  <Image {...gif.imgAttr} />
                   {gif.userDisplayName ? (
                     <aside className={styles.user}>
-                      <img {...gif.userImgAttr} />
+                      <Image {...gif.userImgAttr} />
                       <h5>{gif.userDisplayName}</h5>
                     </aside>
                   ) : null}
--- pages/default.js
+++ pages/default.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
-import Image from "next/image";
 import Head from "next/head";
-import MobileDetect from 'mobile-detect';
+import MobileDetect from "mobile-detect";
 import { prepareGifList } from "../helpers/prepareGifList";
 import styles from "../styles/gif.module.css";
 
@@ -9,16 +8,18 @@ export default function CSR() {
   const [gifs, setGifs] = useState({ data: [], containerHeight: 0 });
 
   useEffect(() => {
-    fetch('https://api.giphy.com/v1/gifs/trending?api_key=xpdrHwpGnEbznpNzYnB4bSAuDAadz8tO')
-      .then(res => res.json())
-      .then(rawGifs => {
+    fetch(
+      "https://api.giphy.com/v1/gifs/trending?api_key=xpdrHwpGnEbznpNzYnB4bSAuDAadz8tO"
+    )
+      .then((res) => res.json())
+      .then((rawGifs) => {
         const md = new MobileDetect(navigator.userAgent);
         let gifs;
-        if(md.mobile()) {
+        if (md.mobile()) {
           gifs = prepareGifList(rawGifs.data, {
             gutter: 14,
             width: 158,
-            defaultColumn: [0,0]
+            defaultColumn: [0, 0],
           });
         } else {
           gifs = prepareGifList(rawGifs.data);
@@ -31,26 +32,26 @@ export default function CSR() {
     <div>
       <div className={styles.container}>
         <Head>
-          <title>CSR in Next.js v10.0.2 Performance Comparison</title>
+          <title>Default (CSR with Pre-renders) in Next.js v10.0.2 Performance Comparison</title>
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <header className={styles.header}>
-          <h1>Client Side Rendering (CSR)</h1>
+          <h1>Default (CSR with Pre-renders)</h1>
           <p>
-            Default Client Side Rendering (CSR) page. All API calls and
-            JavaScript processes happen in the browsers. Although, in Next.js,
-            this is not actually a pure CSR since, by default,{" "}
+            By default,{" "}
             <a href="https://nextjs.org/docs/basic-features/pages">
               Next.js pre-renders every page
             </a>
             . They have{" "}
             <a href="https://nextjs.org/docs/advanced-features/automatic-static-optimization">
               Automatic Static Optimization
-            </a>.
+            </a>
+            . In this implementation, the API call is happening in the browsers.
+            P re-rendering will give the page a better performance.
           </p>
         </header>
         <main>
-          <h2>Trending GIF's in GIPHY</h2>
+          <h2>Trending GIF's in <a href="https://giphy.com">GIPHY</a></h2>
           <div
             className={styles.wrapper}
             style={{
@@ -61,10 +62,10 @@ export default function CSR() {
               return (
                 <section key={gif.id} className={styles.gif} style={gif.style}>
                   <h4>{gif.title}</h4>
-                  <Image {...gif.imgAttr} />
+                  <img {...gif.imgAttr} />
                   {gif.userDisplayName ? (
                     <aside className={styles.user}>
-                      <Image {...gif.userImgAttr} />
+                      <img {...gif.userImgAttr} />
                       <h5>{gif.userDisplayName}</h5>
                     </aside>
                   ) : null}
