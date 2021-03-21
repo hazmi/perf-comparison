48ad03697a83da3a4a3d38402c711adfbea0b8ca
Hazmi Abdun Nazir <hazmi.abdun@bukalapak.com>
2021-03-21 16:25:41 +0700

    add web-vitals report to GA

--- pages/_app.js
+++ pages/_app.js
@@ -11,6 +11,18 @@ const DynamicComponentWithNoSSR = dynamic(
   { ssr: false }
 );
 
+export function reportWebVitals({ id, name, label, value }) {
+  // Use `window.gtag` if you initialized Google Analytics as this example:
+  // https://github.com/vercel/next.js/blob/canary/examples/with-google-analytics/pages/_document.js
+  window.gtag('event', name, {
+    event_category:
+      label === 'web-vital' ? 'Web Vitals' : 'Next.js custom metric',
+    value: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
+    event_label: id, // id unique to current page load
+    non_interaction: true, // avoids affecting bounce rate.
+  })
+}
+
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
   useEffect(() => {
@@ -22,7 +34,7 @@ function MyApp({ Component, pageProps }) {
       router.events.off('routeChangeComplete', handleRouteChange)
     }
   }, [router.events]);
-  
+
   return (
     <>
       <Head>
